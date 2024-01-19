120496ffeda02ace6594d6a158ba9bb818f0ce15
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2024-01-19 20:00:11 +0000

    Fix

--- synapsenet/src/event/block/BlockEvent.php
+++ synapsenet/src/event/block/BlockEvent.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace synapsenet\event\block;
@@ -9,29 +8,5 @@ use synapsenet\world\Position;
 // ??
 class BlockEvent extends Event {
 
-    /** @var Position */
-    protected Position $blockPosition;
-
-    /**
-     * @param Position $blockPosition
-     */
-    public function __construct(Position $blockPosition) {
-        $this->blockPosition = $blockPosition;
-    }
-
-    /**
-     * @return Position
-     */
-    public function getBlockPosition(): Position {
-        return $this->blockPosition;
-    }
-
-    /**
-     * @param Position $blockPosition
-     *
-     * @return void
-     */
-    public function setBlockPosition(Position $blockPosition): void {
-        $this->blockPosition = $blockPosition;
-    }
+   // WIP
 }
